0dbf544ec6a17c0767b55c2a04f8a02e6a848d4f
des-v <desireevaldes@gmail.com>
2021-09-02 06:57:32 +0200

    Fix style errors on game.js

--- game.js
+++ game.js
@@ -1,19 +1,19 @@
 class Game {
-	constructor() {
-		this.score = score
-		this.duration = duration
-		this.level = level
-		this.counter = counter
-		this.playedBy = []
-	}
+  constructor(score, duration, level, counter) {
+    this.score = score
+    this.duration = duration
+    this.level = level
+    this.counter = counter
+    this.playedBy = []
+  }
 
-	setCounter(number) {
-		this.counter = number
-	}
+  setCounter(number) {
+    this.counter = number
+  }
 
-	updateCounter(number) {
-		this.counter = ++number
-	}
+  updateCounter(number) {
+    this.counter = number + 1
+  }
 }
 
 module.exports = Game
