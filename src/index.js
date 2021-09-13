56196082c9e8bedf4b2f6ea2d3140e2a9cdb3e14
des-v <desireevaldes@gmail.com>
2021-09-13 12:58:23 +0200

    Update folder structure on the project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const Person = require('./person')
+const Person = require('./models/person')
 // const Sport = require('./sport')
 // const Game = require('./game')
-const Photo = require('./photo')
+const Photo = require('./models/photo')
 
 const jane = new Person('Jane', 'Doe', 'janedoe@example.com')
 const john = new Person('John', 'Smith', 'johnsmith@example.com')
