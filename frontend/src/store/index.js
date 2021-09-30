008aa6a84d7bd922847d8c6d23b156fe279594aa
des-v <desireevaldes@gmail.com>
2021-09-30 12:16:35 +0200

    Add counter state to store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,42 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
+
+const mutations = {
+  INCREMENT_COUNT: 'increment count',
+  // SET_COUNT: 'set count',
+}
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    count: 0,
+  },
+  mutations: {
+    [mutations.INCREMENT_COUNT](state) {
+      state.count++
+    },
+    // [mutations.SET_COUNT](state) {
+    //   state.count = count
+    // },
+  },
+  actions: {
+    incrementCount({ commit }) {
+      commit(mutations.INCREMENT_COUNT)
+    },
+    // async incrementCount({ commit }) {
+    //   const req = await axios.post(`/api/views`)
+    //   commit(mutations.SET_COUNT, req.data)
+    // },
+    async fetchUser(store, id) {
+      const userRequest = await axios.get(`/api/users/${id}`)
+      return userRequest.data
+    },
+    async fetchUsers() {
+      const userRequest = await axios.get(`/api/users`)
+      return userRequest.data
+    },
+  },
   modules: {},
-});
+})
