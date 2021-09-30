430a45047a2f80335fde0d70d5ec9229d37824ff
des-v <desireevaldes@gmail.com>
2021-09-30 12:08:42 +0200

    Add photos.js file in backend/src/routes folder

---
+++ backend/src/routes/photos.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const router = express.Router()
+const Photo = require('../models/photo')
+
+/* POST create a photo */
+router.post('/', async (req, res) => {
+  const photoToCreate = {
+    filename: req.body.filename,
+  }
+  const createdPhoto = await Photo.create(photoToCreate)
+  res.send(createdPhoto)
+})
+
+module.exports = router
