6f7c96a4a3f2fd3e96478d6d98bcb348191928a2
des-v <desireevaldes@gmail.com>
2021-10-27 21:59:03 +0200

    Add home.vue file in frontend/src/views folder

---
+++ frontend/src/views/home.vue
@@ -0,0 +1,17 @@
+<script>
+    export default {
+        name: 'Home'
+    }
+</script>
+
+<template lang="pug">
+    .home
+        h1 Bonder
+</template>
+
+<style lang="scss" scoped>
+  .home {
+    background-color: rgb(180, 180, 180);
+    padding: 1rem;
+  }
+</style>
