c7baeb56d2274523f2f0de97bd97531db78869f2
des-v <desireevaldes@gmail.com>
2022-01-26 17:48:49 +0100

    Add logo

--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -6,12 +6,30 @@
 
 <template lang="pug">
     .home
+        img(src='img/icons/bonder-logo.png')
         h1 Bonder
+        h2 Welcome 
+        .box
+          h3 Let's bond!
 </template>
 
 <style lang="scss" scoped>
   .home {
     background-color: rgb(180, 180, 180);
     padding: 1rem;
+    h1 {
+      margin: 2rem;
+    }
+    h2 {
+      margin: 3rem;
+    }
+  }
+  .box {
+    background-color: rgb(160, 101, 101);
+    padding: 1rem;
+    h3 {
+      background-color: rgb(119, 101, 160);
+      margin: 4rem;
+    }
   }
 </style>
